6ccd085e7c158f769cc3f7bb81436965c76adbae
Jacopo Gasparetto <jacopo.gasparetto@gmail.com>
2024-07-03 16:50:19 +0200

    Implement GameObject

--- include/isaac/components/game_object.hpp
+++ include/isaac/components/game_object.hpp
@@ -1,10 +1,28 @@
 #ifndef COMPONENTS_GAME_OBJECT_HPP
 #define COMPONENTS_GAME_OBJECT_HPP
 
+#include "physics/vector.hpp"
+#include <vector>
+
 namespace isaac {
 class GameObject
 {
-  
+  vec3 m_position{};
+  bool m_disabled = false;
+  std::vector<GameObject> m_children{};
+
+ protected:
+  virtual void on_start() {};
+  virtual void on_update(float delta) {};
+
+ public:
+  void start();
+  void update(float delta);
+  void enable();
+  void disable();
+  [[nodiscard]] bool enabled() const;
+  void set_position(vec3 position);
+  [[nodiscard]] vec3 get_position() const;
 };
 } // namespace isaac
 #endif
---
+++ src/components/game_object.cpp
@@ -0,0 +1,53 @@
+#include "components/game_object.hpp"
+
+#include <algorithm>
+
+namespace isaac {
+void GameObject::start()
+{
+  std::for_each(m_children.begin(), m_children.end(),
+                [](auto& child) { child.start(); });
+  on_start();
+}
+
+void GameObject::update(float delta)
+{
+  std::for_each(m_children.begin(), m_children.end(),
+                [delta](auto& child) { child.update(delta); });
+  on_update(delta);
+}
+
+void GameObject::enable()
+{
+  std::for_each(m_children.begin(), m_children.end(),
+                [](auto& child) { child.enable(); });
+  m_disabled = false;
+}
+
+void GameObject::disable()
+{
+  std::for_each(m_children.begin(), m_children.end(),
+                [](auto& child) { child.disable(); });
+  m_disabled = true;
+}
+
+bool GameObject::enabled() const
+{
+  return !m_disabled;
+}
+
+void GameObject::set_position(vec3 position)
+{
+  std::for_each(m_children.begin(), m_children.end(),
+                [&, position](auto& child) {
+                  auto const offset = get_position() - child.get_position();
+                  child.set_position(position + offset);
+                });
+  m_position = position;
+}
+
+vec3 GameObject::get_position() const
+{
+  return m_position;
+}
+}
