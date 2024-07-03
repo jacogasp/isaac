b1e793342dd77491c46df4ac9a917dab28e353ca
Jacopo Gasparetto <jacopo.gasparetto@gmail.com>
2024-07-04 00:14:25 +0200

    Add set_scene to SceneManager

--- src/scene/scene_manager.cpp
+++ src/scene/scene_manager.cpp
@@ -1,9 +1,15 @@
 #include "scene/scene_manager.hpp"
+#include <utility>
 
 namespace isaac {
 void SceneManager::create_default_scene()
 {
   m_current_scene = std::make_unique<Scene>();
+}
+
+void SceneManager::set_scene(std::unique_ptr<Scene> scene)
+{
+  m_current_scene = std::move(scene);
 }
 
 Scene* SceneManager::get_current_scene()
