7d2663564e7e2a340b6f4ff9e984a13980b57f43
Jacopo Gasparetto <jacopo.gasparetto@cnaf.infn.it>
2024-07-04 00:14:25 +0200

    Add set_scene to SceneManager

--- include/isaac/scene/scene_manager.hpp
+++ include/isaac/scene/scene_manager.hpp
@@ -9,8 +9,10 @@ namespace isaac {
 class SceneManager
 {
   std::unique_ptr<Scene> m_current_scene;
+
  public:
   void create_default_scene();
+  void set_scene(std::unique_ptr<Scene> scene);
   Scene* get_current_scene();
 };
 } // namespace isaac
